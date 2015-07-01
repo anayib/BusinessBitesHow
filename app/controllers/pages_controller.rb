914cf7d6d621c4e01199ea6ba811419e7d8f6d56
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-01 10:25:07 -0500

    Add new actions, routes and views for video pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, :only => [:welcome_video_user,:welcome_video_subscriptor]
+	before_action :has_subscription?, :only => [:welcome_video_subscriptor]
+
 	def home
 		@courses = Course.all
 		@footer_courses = Course.last(3)
@@ -10,4 +13,17 @@ class PagesController < ApplicationController
 		@footer_courses = Course.last(3)
 		@topic = @course.lessons.first.topics.first
 	end
+
+	def welcome_video_user
+	end
+
+	def welcome_video_subscriptor
+	end
+
+	private
+		def has_subscription?
+	    unless current_user.subscription
+	      redirect_to root_path, :alert => "Acceso denegado, no posee ninguna subscripcion actual para acceder a este contenido"
+	    end
+	  end
 end
