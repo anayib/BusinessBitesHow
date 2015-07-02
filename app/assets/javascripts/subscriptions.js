e00ac52267de1717ef1896e07f3aa47288de5e1c
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-07-02 08:14:39 -0500

    Fix error in stripe form when the user create a new subscription and edit credit card, add scripts for custom forms

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -16,5 +16,24 @@ $(document).on("ready page:load", function() {
     console.log(button.data("id"));
     hidden.val(button.data("id"));  
     console.log(hidden.value);      
-  }); 
+  });
+
+  $('#payment-form').submit(function(event) {
+    var form = $(this);
+    form.find('button').prop('disabled', true);
+    Stripe.createToken(form, stripeResponseHandler);
+    return false;
+  });
+
+  function stripeResponseHandler(status, response) {
+    var form = $('#payment-form');
+    if (response.error) {
+      form.find('.payment-errors').text(response.error.message);
+      form.find('button').prop('disabled', false);
+    } else {
+      var token = response.id;
+      form.append($('<input type="hidden" name="stripeToken">').val(token));
+      form.get(0).submit();
+    }
+  } 
 });
