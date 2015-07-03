be65c398f57fbed588c144c11fe6e42d22c85369
alejoescobar <alejoescobac@gmail.com>
2015-07-02 19:37:42 -0500

    Add two column premium subscription

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,23 +10,39 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(user)
-    unless params[:course_id].empty?
+    if (params.has_key?(:course_id))
       course = Course.find(params[:course_id]) if params[:course_id]
     end
   	if user_signed_in? && current_user.sign_in_count == 1
-      unless params[:course_id].empty?
+      ####
+      # unless params[:course_id].empty?
+      #   if course.course_type == "with_charge"
+      #     new_subscription_path(:subscription_plan => params[:subscription_plan])
+      #   else
+      # 		welcome_new_user_path
+      #   end
+      # else
+      #   if params[:subscription] == true
+      #     new_subscription_path
+      #   else
+      #     welcome_new_user_path
+      #   end
+      # end
+      ####
+      if (params.has_key?(:course_id))
         if course.course_type == "with_charge"
-          new_subscription_path
+          new_subscription_path(:subscription_plan => params[:subscription_plan])
         else
-      		welcome_new_user_path
+          welcome_new_user_path
         end
       else
-        if params[:subscription] == true
+        if (params.has_key?(:subscription))
           new_subscription_path
         else
           welcome_new_user_path
         end
       end
+      ####
   	else
     	session[:previous_url] || root_path
   	end
