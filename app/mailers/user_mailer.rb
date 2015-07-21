ee68afe531cbbb4cfcf07fe567ece32f2e72fa59
anayib <nayib.abdala@gmail.com>
2015-07-20 23:51:46 -0500

    Add mailing copy. Missing url to share - viral strategy for new accounts created- and confirmation of @url route

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,13 +3,13 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://www.businessbites.how/'
-    mail(to: @user.email, subject: 'Bienvenido a Business Bites')
+    mail(to: @user.email, subject: 'Bienvenido a BusinessHackers!')
   end
 
   def welcome_subscriptor_email(user)
     @user = user
     @url  = 'http://www.businessbites.how/'
-    mail(to: @user.email, subject: 'Felicitaciones por tu suscripción. Bienvenido Business Bites VIP')
+    mail(to: @user.email, subject: 'Bienvenido a BusinessHackers VIP!')
   end
 
 end
