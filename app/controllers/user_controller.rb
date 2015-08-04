6667dbbc2bc1cb3339e4b1e54c86752013b5869f
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-08-04 15:33:58 -0500

    Add new actions to users control - New - Create

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,6 +6,22 @@ class UserController < ApplicationController
 		@users = User.all
 	end
 
+	def new
+		  @user = User.new
+		end
+
+	def create
+  	@user = User.new(user_params)
+	  if @user.save
+	  	UserMailer.welcome_email(@user).deliver_now
+  	  flash[:notice] = "El usuario #{@user.email} fue creado con exito"
+  	  redirect_to user_index_path
+  	else
+  		flash[:alert] = "Ha ocurrido un error y el usuario #{@user.email}, no ha sido almacenado"
+    	render :action => 'new'
+  	end
+	end
+
 	def edit
 		@user = User.find(params[:id])
 	end
