1fa2cbb5bf0fa294f6ce814880779c7bcd84c483
Sebastian Zapata Mardini <sezama03@hotmail.com>
2015-08-11 11:33:49 -0500

    Add copie and subjet for welcome guest user email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,19 +3,19 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'http://www.businesshackers.how/'
-    mail(to: @user.email, subject: 'Bienvenido a BusinessHackers!')
+    mail(to: @user.email, subject: 'Bienvenido a Business Hackers!')
   end
 
   def welcome_guest_email(user)
     @user = user
     @url  = 'http://www.businesshackers.how/'
-    mail(to: @user.email, subject: 'Bienvenido a BusinessHackers!')
+    mail(to: @user.email, subject: 'Bienvenido a Business Hackers!')
   end
 
   def welcome_subscriptor_email(user)
     @user = user
     @url  = 'http://www.businesshackers.how/'
-    mail(to: @user.email, subject: 'Bienvenido a BusinessHackers VIP!')
+    mail(to: @user.email, subject: 'Bienvenido a Business Hackers VIP!')
   end
 
   def end_guest_account(user)
